e114209e4110b59fc9c8f3b669994054db511fd2
Kenzo <z5209433@student.unsw.edu.au>
2020-03-19 14:03:33 +1100

    Initial commit

---
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailActivity.java
@@ -0,0 +1,32 @@
+package au.edu.unsw.infs3634.cryptobag;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+        int position = intent.getIntExtra(MainActivity.EXTRA_MESSAGE, 0);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        Fragment fragment = new DetailFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.scrollView, fragment).commit();
+    }
+}
